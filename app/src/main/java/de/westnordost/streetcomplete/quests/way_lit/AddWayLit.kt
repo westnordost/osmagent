2312e33a14958f141485a960ebaeba2bb13b2f09
JayTurnr <jaynicholasturner@gmail.com>
2021-05-11 18:36:42 +0100

    Add to other lit quests

--- app/src/main/java/de/westnordost/streetcomplete/quests/way_lit/AddWayLit.kt
+++ app/src/main/java/de/westnordost/streetcomplete/quests/way_lit/AddWayLit.kt
@@ -5,6 +5,7 @@ import de.westnordost.streetcomplete.data.meta.MAXSPEED_TYPE_KEYS
 import de.westnordost.streetcomplete.data.meta.updateWithCheckDate
 import de.westnordost.streetcomplete.data.osm.osmquests.OsmFilterQuestType
 import de.westnordost.streetcomplete.data.osm.edits.update_tags.StringMapChangesBuilder
+import de.westnordost.streetcomplete.data.quest.OnlyNight
 
 class AddWayLit : OsmFilterQuestType<WayLit>() {
 
@@ -42,6 +43,7 @@ class AddWayLit : OsmFilterQuestType<WayLit>() {
     override val wikiLink = "Key:lit"
     override val icon = R.drawable.ic_quest_lantern
     override val isSplitWayEnabled = true
+    override val dayNightCycle = OnlyNight
 
     override fun getTitle(tags: Map<String, String>): Int {
         val type = tags["highway"]
